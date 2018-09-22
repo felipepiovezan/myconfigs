c8de27c3bb651312357000a0b23b8ce2fa8402fb
Felipe A. Piovezan <piovezan.fpi@gmail.com>
2018-09-22 16:33:02 -0400

    [vim] change install to make lns with absolute paths

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,9 @@
 set -x
 
-cp .vimrc $HOME/.vimrc
-cp -r syntax/ $HOME/.vim/
-cp -r pack/ $HOME/.vim/
+rm $HOME/.vimrc
+rm -r $HOME/.vim/syntax
+rm -r $HOME/.vim/pack
+
+ln -s $PWD/.vimrc $HOME/.vimrc
+ln -s $PWD/syntax/ $HOME/.vim/
+ln -s $PWD/pack/ $HOME/.vim/
