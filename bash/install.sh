f69625a0c9fe209c48d3cd406fde2551bc530148
Felipe de Azevedo Piovezan <felipe.de.azevedo.piovezan@intel.com>
2019-10-26 16:18:22 -0400

    Fix bashrc install to use PWD with ln

--- bash/install.sh
+++ bash/install.sh
@@ -7,6 +7,6 @@ fi
 
 rm ~/.bash_profile ~/.bashrc_pc_specific "$1"
 
-ln --symbolic bash_profile ~/.bash_profile
-ln --symbolic bashrc_pc_specific ~/.bashrc_pc_specific
-ln --symbolic bashrc "$1"
+ln --symbolic $PWD/bash_profile ~/.bash_profile
+ln --symbolic $PWD/bashrc_pc_specific ~/.bashrc_pc_specific
+ln --symbolic $PWD/bashrc "$1"
