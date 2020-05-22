1cc058a09a1986ce4e51ee51c3f11bf362e0b15c
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-05-22 12:52:14 -0400

    update Xorg defaults

--- xorg/install.sh
+++ xorg/install.sh
@@ -4,6 +4,6 @@ rm $HOME/.Xdefaults
 rm $HOME/.Xresources
 rm $HOME/.Xresources.dark
 
-ln --symbolic $PWD/.Xdefaults      $HOME/
-ln --symbolic $PWD/.Xresources      $HOME/
-ln --symbolic $PWD/.Xresources.dark $HOME/
+ln --symbolic $PWD/Xdefaults      $HOME/.Xdefaults
+ln --symbolic $PWD/Xresources      $HOME/.Xresources
+ln --symbolic $PWD/Xresources.dark $HOME/.Xresources.dark
