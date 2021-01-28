f9ef4afff2fd750bbe94ab5843363503a90252d1
Felipe de Azevedo Piovezan <felipe@cerebras.net>
2021-01-28 06:06:33 -0800

    Add csl to vim

--- vim/install.sh
+++ vim/install.sh
@@ -6,6 +6,7 @@ rm -r $HOME/.vim/pack
 rm -r $HOME/.vim/colors
 
 ln --symbolic $PWD/vimrc $HOME/.vimrc
+ln --symbolic $PWD/ftdetect $HOME/.vimrc
 ln --symbolic $PWD/syntax/ $HOME/.vim/
 ln --symbolic $PWD/pack/ $HOME/.vim/
 ln --symbolic $PWD/colors/ $HOME/.vim/
