1ebff217eecfafc46726ccc758fd6a9c33b4c398
Felipe de Azevedo Piovezan <felipe@cerebras.net>
2021-06-29 07:48:17 -0700

    [vim] add more directories

--- vim/install.sh
+++ vim/install.sh
@@ -4,9 +4,14 @@ rm $HOME/.vimrc
 rm -r $HOME/.vim/syntax
 rm -r $HOME/.vim/pack
 rm -r $HOME/.vim/colors
+rm -r $HOME/.vim/ftdetect
+rm -r $HOME/.vim/ftplugin
+rm -r $HOME/.vim/indent
 
 ln -s $PWD/vimrc $HOME/.vimrc
 ln -s $PWD/syntax/ $HOME/.vim/
 ln -s $PWD/pack/ $HOME/.vim/
 ln -s $PWD/colors/ $HOME/.vim/
-ln -s $PWD/ftdetect $HOME/.vimrc
+ln -s $PWD/ftdetect $HOME/.vim/
+ln -s $PWD/ftplugin $HOME/.vim/
+ln -s $PWD/indent $HOME/.vim/
